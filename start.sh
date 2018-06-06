cbf98594713a7c9776e75b839e506ed2e80d55c1
Ben Fairfield <benfa@umich.edu>
2018-06-06 15:38:53 -0400

    change start.sh to be compatible with RHEL system

--- start.sh
+++ start.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # Redirect logs to stdout and stderr for docker reasons.
-# it seems both sylinks already exist. 
-# these commands create unnecessary duplicates
-ln -sf /dev/stdout /var/log/apache2/access_log
-ln -sf /dev/stderr /var/log/apache2/error_log
+ln -sf /dev/stdout /var/apache2/log/access_log
+ln -sf /dev/stderr /var/apache2/log/error_log
 
 # apache and virtual host secrets
-ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
-ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
-ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
+ln -sf /secrets/httpd/httpd.conf /etc/httpd/conf/httpd.conf
+#ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+ln -sf /secrets/httpd/cosign.conf /etc/httpd/conf.d/cosign.conf
 
 # SSL secrets
-ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
-ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
-ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
+ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/pki/tls/private/USERTrustRSACertificationAuthority.pem
+ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/pki/tls/private/AddTrustExternalCARoot.pem
+ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/pki/tls/private/sha384-Intermediate-cert.pem
 
 if [ -f /secrets/app/local.start.sh ]
 then
@@ -22,14 +20,11 @@ then
 fi
 
 ## Rehash command needs to be run before starting apache.
-c_rehash /etc/ssl/certs >/dev/null
+c_rehash /etc/pki/tls/certs >/dev/null
 
-a2enmod ssl
-a2enmod include
-a2ensite default-ssl 
+cd /var/www/html
 
-#cd /var/www/html
 #drush @sites cc all --yes
 #drush up --no-backup --yes
 
-/usr/local/bin/apache2-foreground
+service httpd start
