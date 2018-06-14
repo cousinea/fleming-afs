7ffe308df20e35f7db6e743568612fd084074b3f
Ben Fairfield <benfa@umich.edu>
2018-06-14 10:56:49 -0400

    Edited Dockerfile

--- start.sh
+++ start.sh
@@ -28,4 +28,6 @@ c_rehash /etc/pki/tls/certs >/dev/null
 
 #while true; do sleep 2; done
 
+#systemctl start httpd.service || systemctl status httpd.service
+
 /usr/sbin/httpd -DFOREGROUND -f "/etc/httpd/conf/httpd.conf"
